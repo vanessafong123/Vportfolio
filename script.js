f73344b7a50f69ff75ae30d0f279ee278ea58fe3
vanessafong123 <143882770+vanessafong123@users.noreply.github.com>
2023-09-29 17:22:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.header');
   const navbar = header.querySelector('.navbar');
-  const menuButton = header.querySelector('.menu');
+  const menuButton = header.querySelector('.menu-icon');
   const backToTopButton = document.getElementById('myBtn');
 
-  // header scroll
   window.addEventListener('scroll', () => {
     header.classList.toggle('active', window.scrollY > 0);
-    navbar.classList.remove('show');
     backToTopButton.style.display = window.scrollY > 20 ? 'block' : 'none';
   });
 
-  // responsive menu
   menuButton.addEventListener('click', () => {
     navbar.classList.toggle('show');
   });
+  document.onscroll = () => {
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
 
-  // back to top
+document.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
   backToTopButton.addEventListener('click', () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   });
 });
-
